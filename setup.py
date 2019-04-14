ea058b6cec08439e1e27ce6e7b066ee0ebff3bc6
Dmitry Rodin <madiedinro@gmail.com>
2019-04-15 02:54:07 +0300

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setup(
     name='rodin_helpers',
-    version='0.1.0',
+    version='0.1.1',
     author='Dmitry Rodin',
     author_email='madiedinro@gmail.com',
     license='MIT',
